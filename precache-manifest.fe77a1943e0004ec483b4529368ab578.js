8c81be438d4ab7209466be23a0126d3970f090f2
musavvir.noor <musavvir.noor@gmail.com>
2020-01-28 16:59:07 -0500

    Updates

---
+++ precache-manifest.fe77a1943e0004ec483b4529368ab578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1744207422964433ebe74f82b6fd67a1",
+    "url": "/aoe2Lobby/index.html"
+  },
+  {
+    "revision": "c1f642e6866d2f3d6b4d",
+    "url": "/aoe2Lobby/static/css/main.655f66f1.chunk.css"
+  },
+  {
+    "revision": "68ce5803258493333e9f",
+    "url": "/aoe2Lobby/static/js/2.5e03ec0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/aoe2Lobby/static/js/2.5e03ec0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1f642e6866d2f3d6b4d",
+    "url": "/aoe2Lobby/static/js/main.937e1f80.chunk.js"
+  },
+  {
+    "revision": "59fc4d9f1e080085ed0b",
+    "url": "/aoe2Lobby/static/js/runtime-main.ecb9ac36.js"
+  }
+]);
