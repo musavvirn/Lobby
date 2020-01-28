b39df297feea3182cab6c96c7de5319ea2a4c635
musavvir.noor <musavvir.noor@gmail.com>
2020-01-28 17:09:52 -0500

    update

---
+++ build/precache-manifest.9a837eb3e1faadfc17094ba936d7eb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f196408cee73f89cda4dbcf82740c5",
+    "url": "/Lobby/index.html"
+  },
+  {
+    "revision": "0502e01116599e65cb1d",
+    "url": "/Lobby/static/css/main.2e4155bc.chunk.css"
+  },
+  {
+    "revision": "68ce5803258493333e9f",
+    "url": "/Lobby/static/js/2.5e03ec0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Lobby/static/js/2.5e03ec0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0502e01116599e65cb1d",
+    "url": "/Lobby/static/js/main.2873db1d.chunk.js"
+  },
+  {
+    "revision": "882e0e73f9825559ff96",
+    "url": "/Lobby/static/js/runtime-main.7f005f24.js"
+  }
+]);
