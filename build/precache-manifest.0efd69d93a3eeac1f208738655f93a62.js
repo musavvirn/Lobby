c2e1617d3c05061b3d77cc976a1340b88c8e14fc
musavvir <musavvir.noor@gmail.com>
2021-10-09 00:05:26 -0700

    DE lobbies update - changed the lobby service request API

--- build/precache-manifest.0efd69d93a3eeac1f208738655f93a62.js
+++ build/precache-manifest.0efd69d93a3eeac1f208738655f93a62.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c9e7c45d1e9b4e6586c979dc3770696",
+    "revision": "22eb57d68cc5c5666c5d15578a882360",
     "url": "/Lobby/index.html"
   },
   {
-    "revision": "d8a8dac6ba4981eac06f",
-    "url": "/Lobby/static/css/main.102c4ba6.chunk.css"
+    "revision": "a3cedd0c88821d500d74",
+    "url": "/Lobby/static/css/main.c3e765ac.chunk.css"
   },
   {
     "revision": "6ec6d9937dbec724b87e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Lobby/static/js/2.f4e7f14a.chunk.js.LICENSE"
   },
   {
-    "revision": "d8a8dac6ba4981eac06f",
-    "url": "/Lobby/static/js/main.f7046111.chunk.js"
+    "revision": "a3cedd0c88821d500d74",
+    "url": "/Lobby/static/js/main.5aa2cae2.chunk.js"
   },
   {
     "revision": "882e0e73f9825559ff96",
